55d487c344ba6e444a908ce873a4e3f173725cf0
basilio007 <113118989+basilio007@users.noreply.github.com>
2022-11-13 21:18:24 +0100

    almosdone

---
+++ Move objects with keys/Assets/Scrips/Movercubo1.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movercubo1 : MonoBehaviour
+{
+public Rigidbody cubo1;
+public Rigidbody cubo2;
+    public Rigidbody cubo3;
+    public Rigidbody esfera1;
+    public Rigidbody esfera2;
+
+    void Update()
+    {
+       //movimientosd del cubo1
+        if (Input.GetKey(KeyCode.Z))
+        {
+            cubo1.AddForce(0, 0, 30);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            cubo1.AddForce(0, 0, -30);
+        }
+       else if (Input.GetKey(KeyCode.A))
+        {
+            cubo1.AddForce(0, 30, 0);
+        }
+
+
+        //movimientosd del cubo2
+        if (Input.GetKey(KeyCode.X))
+        {
+            cubo2.AddForce(0, 0, 30);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            cubo2.AddForce(0, 0, -30);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            cubo2.AddForce(0, 30, 0);
+        }
+
+        //movimientosd del cubo3
+        if (Input.GetKey(KeyCode.C))
+        {
+            cubo3.AddForce(0, 0, 30);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            cubo3.AddForce(0, 0, -30);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            cubo3.AddForce(0, 30, 0);
+        }
+
+        //movimientosd del sfera1
+        if (Input.GetKey(KeyCode.V))
+        {
+            esfera1.AddForce(0, 0, 30);
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            esfera1.AddForce(0, 0, -30);
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            esfera1.AddForce(0, 30, 0);
+        }
+
+        //movimientosd del sfera1
+        if (Input.GetKey(KeyCode.B))
+        {
+            esfera2.AddForce(0, 0, 30);
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            esfera2.AddForce(0, 0, -30);
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            esfera2.AddForce(0, 30, 0);
+        }
+    }
+}
